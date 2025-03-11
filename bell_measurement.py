4c163f8f11f0082911df2defee8f81779b80f7c8
hayacon <hayacon13@gmail.com>
2025-03-11 11:57:00 +0000

    bell measurement

--- bell_measurement.py
+++ bell_measurement.py
@@ -1,25 +1,81 @@
 import numpy as np
 
-
 class Bell_measurement:
     def __init__(self, state_1, state_2, basis_1, basis_2):
         self.state_1 = state_1
         self.state_2 = state_2
+        self.outcome_1 = []
+        self.outcome_2 = []
+        self.result = []
         #check if for variables are the same length, if not return an error
         assert len(self.state_1) == len(self.state_2), "Variables must have the same length"
 
     def beam_spliter(self, state):
         ''' 50/50 beam splitter matrix '''
-        pass
-
-    def polarization_beam_spliter(self, state):
         if state == 'H':
-            return 'H'
+            return np.random.choice(['H', 'V'], p=[0.5, 0.5])
         elif state == 'V':
+            return np.random.choice(['H', 'V'], p=[0.5, 0.5])
+        elif state == 'D':
+            return 'H'
+        elif state == 'A':
             return 'V'
+        else:
+            return 'Invalid state'
 
-    def detector(self):
-        pass
+    def polarization_beam_spliter(self, state, user_side: str):
+        ''' 
+        Polarization beam splitter matrix 
+        user_side: 'Alice' or 'Bob'
+        
+        '''
+        if user_side == 'Alice':
+            if state == 'H':
+                return 'D2h'
+            elif state == 'V':
+                return 'D1v'
+        elif user_side == 'Bob':
+            if state == 'H':
+                return 'D1h'
+            elif state == 'V':
+                return 'D2v'
+
+    def measurement(self):
+        ''' 
+        perform bell state measurement
+        '''
+        for i in range(len(self.state_1)):
+            state_1 = self.beam_spliter(self.state_1[i])
+            state_2 = self.beam_spliter(self.state_2[i])
+            outcome_1 = self.polarization_beam_spliter(state_1, 'Alice')
+            outcome_2 = self.polarization_beam_spliter(state_2, 'Bob')
+            self.outcome_1.append(outcome_1)
+            self.outcome_2.append(outcome_2)
+
+        return self.outcome_1, self.outcome_2
+
+    def annunce_result(self):
+        ''' 
+        Announce the result of the measurement
+        ---
+        D1h, D1v:|psi+>
+        D2h, D2v:|psi+>
+        D1h, D2v:|psi->
+        D2h, D1v:|psi->
+        '''
+        for i in range(len(self.outcome_1)):
+            if self.outcome_1[i] == 'D1h' and self.outcome_2[i] == 'D1v':
+                self.result.append('|psi+>')
+            elif self.outcome_1[i] == 'D2h' and self.outcome_2[i] == 'D2v':
+                self.result.append('|psi+>')
+            elif self.outcome_1[i] == 'D1h' and self.outcome_2[i] == 'D2v':
+                self.result.append('|psi->')
+            elif self.outcome_1[i] == 'D2h' and self.outcome_2[i] == 'D1v':
+                self.result.append('|psi->')
+            else:
+                self.result.append('Fail')
+        return self.result
+
     
 
 
--- test_bell_measurement.py
+++ test_bell_measurement.py
@@ -1,34 +1,35 @@
 import pytest
 import numpy as np
-
 from bell_measurement import Bell_measurement
-from user import User
-
 
 def test_init():
-    """
-    Test that the BellMeasurement object initializes with the correct bit_len
-    and an empty random_bits list.
-    """
-    user_1 = User(10)
-    user_2 = User(10)
-    basis_1 = user_1.set_basis()
-    basis_2 = user_2.set_basis()
-    state_1 = user_1.state_encoder()
-    state_2 = user_2.state_encoder()
-    b = Bell_measurement(state_1, state_2, basis_1, basis_2)
-    assert isinstance(b.state_1, list)
-    assert isinstance(b.state_2, list)
-    assert len(b.state_1) == len(b.state_2)
+    state_1 = ['H', 'V']
+    state_2 = ['H', 'V']
+    basis_1 = ['H', 'V']
+    basis_2 = ['H', 'V']
+    bm = Bell_measurement(state_1, state_2, basis_1, basis_2)
+    assert bm.state_1 == state_1
+    assert bm.state_2 == state_2
+    assert bm.outcome_1 == []
+    assert bm.outcome_2 == []
+
+    with pytest.raises(AssertionError):
+        Bell_measurement(['H'], ['H', 'V'], basis_1, basis_2)
 
 def test_beam_spliter():
-    """
-    Test that the beam_spliter method returns the correct matrix
-    """
-    user_1 = User(10)
-    user_2 = User(10)
-    basis_1 = user_1.set_basis()
-    basis_2 = user_2.set_basis()
-    state_1 = user_1.state_encoder()
-    state_2 = user_2.state_encoder()
-    b = Bell_measurement(state_1, state_2, basis_1, basis_2)
+    bm = Bell_measurement(['H'], ['H'], ['H'], ['H'])
+    assert bm.beam_spliter('H') in ['H', 'V']
+    assert bm.beam_spliter('V') in ['H', 'V']
+    assert bm.beam_spliter('D') == 'H'
+    assert bm.beam_spliter('A') == 'V'
+    assert bm.beam_spliter('X') == 'Invalid state'
+
+def test_polarization_beam_spliter():
+    bm = Bell_measurement(['H'], ['H'], ['H'], ['H'])
+    assert bm.polarization_beam_spliter('H', 'Alice') == 'D2h'
+    assert bm.polarization_beam_spliter('V', 'Alice') == 'D1v'
+    assert bm.polarization_beam_spliter('H', 'Bob') == 'D1h'
+    assert bm.polarization_beam_spliter('V', 'Bob') == 'D2v'
+
+def test_announce_result():
+    pass
